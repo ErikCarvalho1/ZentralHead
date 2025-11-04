d1811508320139eb540a97872eea572df9d65990
ERIK CARVALHO DA SILVA <erik.csilva7@senacsp.edu.br>
2025-11-04 09:37:13 -0300

    pj_produtos atualizados

--- php/clientes/pagina_produto.php
+++ php/clientes/pagina_produto.php
@@ -20,17 +20,16 @@ if(!$produto){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>pagina do produto</title>
-
-    <!-- Bootstrap -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-  </head>
-  <bory>
-  <?php include "cabecalho.php"; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script> 
+  <title>Document</title>
+
+</head>
+<body>
+   <nav><?php include "cabecalho.php"; ?></nav> <main>
 
 <div class="container mt-5">
   <div class="row">
@@ -88,8 +87,9 @@ if(!$produto){
     </div>
   </div>
 </div>
-
- <?php include "../menu_publico/rodape.php"?> 
+</main>
+ <footer><?php include "../menu_publico/rodape.php"?> </footer>
+ 
 
 </body>
 </html>
